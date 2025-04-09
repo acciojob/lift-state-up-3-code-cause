9ea6d6a48b0fec40dd4ccf5ce0f2cf93452075d4
Ashish <writeashish22@gmail.com>
2025-04-09 17:40:40 +0530

    li3

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
-import Parent from "./Parent";
+
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
   return (
-    <div>
-      {/* Do not remove the main div */}
-      <Parent />
+    <div className="Parent" style={{ backgroundColor: "red", padding: "20px" }}>
+      <h1>Parent Component</h1>
+
+      <ChildComponent1 onSelect={setSelectedOption} />
+      <ChildComponent2 onSelect={setSelectedOption} />
+
+      <p>Selected Option: {selectedOption}</p>
     </div>
   );
 };
+
 
 export default App;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import ChildComponent1 from "./ChildComponent1";
-import ChildComponent2 from "./ChildComponent2";
+// import React, { useState } from "react";
+// import ChildComponent1 from "./ChildComponent1";
+// import ChildComponent2 from "./ChildComponent2";
 
-const Parent = () => {
-  const [selectedOption, setSelectedOption] = useState('');
+// const App = () => {
+//   const [selectedOption, setSelectedOption] = useState('');
 
-  return (
-    <div className="Parent" style={{ backgroundColor: "red", padding: "20px" }}>
-      <h1>Parent Component</h1>
+//   return (
+//     <div className="Parent" style={{ backgroundColor: "red", padding: "20px" }}>
+//       <h1>Parent Component</h1>
 
-      <ChildComponent1 onSelect={setSelectedOption} />
-      <ChildComponent2 onSelect={setSelectedOption} />
+//       <ChildComponent1 onSelect={setSelectedOption} />
+//       <ChildComponent2 onSelect={setSelectedOption} />
 
-      <p>Selected Option: {selectedOption}</p>
-    </div>
-  );
-};
+//       <p>Selected Option: {selectedOption}</p>
+//     </div>
+//   );
+// };
 
-export default Parent;
+// export default App;
